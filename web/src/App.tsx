f2e1635a66c81927c7f5d2a0451fdb61d87b35dc
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-17 21:13:45 -0300

    projeto parado por falta de conhecimento NLW.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
-
+import "./styles/global.css"
 export function App() {
 
   return (
     <>
-      <h1>teste</h1>
+      <h1 className="bg-zinc-800 w-10 h-10 text-white rounded m-2">teste</h1>
     </>)
 }
 
